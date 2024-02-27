6be33d86cdb25ac1066d974b0e17000cd2be3bf5
DavidHundertpfund <david.hundertpfund@hak-reutte.ac.at>
2024-02-27 12:10:28 +0100

    test

---
+++ src/arena/Dragon.java
@@ -0,0 +1,11 @@
+package arena;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Dragon extends Charakter{
+
+	public int randompoints() {
+	ThreadLocalRandom.current().nextInt(20, 25+1);
+		
+	}
+	
+}
